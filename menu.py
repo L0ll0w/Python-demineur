78f14ff0b42b8479c4872e159e00d09c0128876f
noasman971 <noah.kichenassamy@edu.esiee-it.fr>
2024-12-16 15:55:11 +0100

    add main screen

--- menu.py
+++ menu.py
@@ -1,11 +1,34 @@
 import pygame
+
 pygame.init()
-ecran = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-ecran.fill((136, 162, 193))
-pygame.display.flip()
-continuer = True
-while continuer:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            continuer = False
-pygame.quit()
+
+screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+screen.fill((136, 162, 193))
+font = pygame.font.Font("font/Super Sense.ttf", 100)
+titre = font.render("Des mineurs", 1, (255, 255, 255))
+boutton_play = pygame.image.load("image/play.png").convert_alpha()
+boutton_play = pygame.transform.scale(boutton_play, (1200, 1200))
+rect = boutton_play.get_rect()
+
+
+class Menu:
+
+    def display(self, screen , text):
+        ingame = True
+        while ingame:
+            mouse = pygame.mouse.get_pos()
+            screen.blit(text, (650,100))
+            screen.blit(boutton_play, (400,50))
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN and rect.collidepoint(mouse):
+                    screen.blit(font.render("JADORE LES MINEURS", 1, (255,255,255)), (100,1000))
+                    pygame.display.flip()
+                if event.type == pygame.MOUSEBUTTONDOWN and not rect.collidepoint(mouse):
+                    pygame.quit()
+
+            pygame.display.flip()
+        pygame.quit()
+
+
+menu = Menu()
+menu.display(screen, titre)
